7ecffe3117996464b97b7f44d492465b82d8fb9f
generiglesias <giglesia@sfu.ca>
2017-01-23 01:09:41 -0800

    Add plain html page for each project and links now redirect to them

--- js/index.js
+++ js/index.js
@@ -1,3 +1,17 @@
 <script>
+	document.getElementById("AWSbutton").onclick = function () {
+        location.href = "awsdynamo.html";
+    };
 
+    document.getElementById("RPGButton").onclick = function () {
+        location.href = "textbasedrpg.html";
+    };
+
+    document.getElementById("AndroidButton").onclick = function () {
+        location.href = "eventmanager.html";
+    };
+
+    document.getElementById("LegoButton").onclick = function () {
+        location.href = "legomaze.html";
+    };
 </script>
